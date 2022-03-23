8c8ea935d6b124169d27c755a23e006b2151bc7b
deadlyaman <deadlyaman>
2022-03-23 23:57:16 +0530

    Add

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ db.sequelize.sync();
 //db.sequelize.sync({force : true});
 
 const PORT = process.env.PORT || 8081; 
+
 //start the server
 app.listen(PORT,console.log(`server started in ${process.env.NODE_ENV} mode at port ${PORT}`));
 app.get('/',(req,res) => res.send({data : 1}))
-// fetch routes
+
+//routes
 const auth = require("./routes/auth.routes");
 app.use('/',auth);
 const Tweet = require("./routes/Tweets.route");
